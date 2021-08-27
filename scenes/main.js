360df1660e20487e1bb68def8db0e7f1747cdece
James Dobbe <dobbe2@gmail.com>
2021-08-27 23:15:29 +0000

    ship is moving, bullets are firing, updated ship sprite

--- scenes/main.js
+++ scenes/main.js
@@ -1,7 +1,12 @@
 const MOVE_SPEED = 200;
+const INVADER_SPEED = 100
+let CURRENT_SPEED = INVADER_SPEED
+const LEVEL_DOWN = 100
 
+//layers of the game board
 layer(['obj', 'ui'], 'obj')
 
+//create the game board
 addLevel([
   '!^@^@^@^@^@    &',
   '!@^@^@^@^@^    &',
@@ -12,29 +17,57 @@ addLevel([
   '!              &',
   '!              &',
   '!              &',
+  '!              &',
+  '!              &',
 ], {
   width: 30,
   height: 22,
   '^' : [ sprite('space-invader'), scale(0.7), 'space-invader'],
-  '@' : [sprite('space-invader2'), scale(0.7), 'space-invader'],
-  '!' : [ sprite('wall'), 'left-wall', ],
+  '@' : [ sprite('space-invader2'), scale(0.7), 'space-invader'],
+  '!' : [ sprite('wall'), 'left-wall'],
   '&' : [ sprite('wall'), 'right-wall']
   
 })
 
+//add spaceship
 const player = add([
   sprite('space-ship'),
-  pos(width() / 2, height() / 2),
+  pos(width() / 2, height() * .8),
   origin('center')
 ]);
 
-
+//move player left
 keyDown('left', () => {
   player.move(-MOVE_SPEED, 0)
 })
 
+//move player right
 keyDown('right', () => {
   player.move(MOVE_SPEED, 0)
+})
+
+//spawn bullet
+function spawnBullet(p){
+  add([
+    rect(4,12), 
+    pos(p), 
+    origin('center'), 
+    color(0.5, 0.5, 1), 
+    'bullet'
+    ])
+}
+//fire bullets
+keyPress('space', () => {
+  spawnBullet(player.pos.add(0, -17))
+})
+
+const BULLET_SPEED = 400
+//move bullet
+action('bullet', (b) => {
+  b.move(0, -BULLET_SPEED)
+  if(b.pos.y < 0) {
+    destroy(b);
+  }
 })
 
 const score = add([
@@ -68,8 +101,32 @@ const score = add([
 // }
 // })
 
-const INVADER_SPEED = 100
+action('space-invader', (s) => {
+  s.move(CURRENT_SPEED, 0)
+})
+
+//right wall collision and reverse
+collides('space-invader', 'right-wall', () => {
+  CURRENT_SPEED = -INVADER_SPEED
+  every('space-invader', (s) => {
+     s.move(0, LEVEL_DOWN)
+  })
+})
+
+//left wall collision and reverse
+collides('space-invader', 'left-wall', () => {
+  CURRENT_SPEED = INVADER_SPEED
+  every('space-invader', (s) => {
+     s.move(0, LEVEL_DOWN)
+  })
+})
+
+player.overlaps('space-invader', () => {
+  go('lose', { score : score.value })
+})
 
 action('space-invader', (s) => {
-  s.move(INVADER_SPEED, 0)
+  if (s.pos.y >= height() /2 ){
+      go('lose', { score : score.value })
+  }
 })
