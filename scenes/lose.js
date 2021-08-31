b27eddb0fcafe4276db79a22a1ddf587f6f9eab3
James Dobbe <dobbe2@gmail.com>
2021-08-31 07:10:55 +0000

    sound effects added, walls created, added more stars

--- scenes/lose.js
+++ scenes/lose.js
@@ -1,6 +1,21 @@
+const endMusic = play('gameOver');
+
 add([
   text(`YOU LOSE! Score:`.trim() + args.score),
   origin('center'),
-  scale(10),
-  pos(width() / 2, height() / 2)
+  scale(3),
+  pos(width() / 2, height() / 3)
 ])
+
+add([
+  text(`Press Space to play again`),
+  origin('center'),
+  scale(2),
+  pos(width() / 2, height() /2)
+]);
+
+keyPress("space", () => {
+
+	go("main")
+
+});
--- scenes/win.js
+++ scenes/win.js
@@ -1,7 +1,22 @@
+const winMusic = play('win');
+
 add([
   text(`WINNER! Score:`.trim() + args.score),
   // text("Press Space to Start Again"),
   origin('center'),
   scale(3),
-  pos(width() / 2, height() / 2)
+  pos(width() / 2, height() / 3)
 ]);
+
+add([
+  text(`Press Space to play again`),
+  origin('center'),
+  scale(2),
+  pos(width() / 2, height() /2)
+]);
+
+keyPress("space", () => {
+
+	go("main")
+
+});
