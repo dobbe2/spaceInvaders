28a5420d77bd077ccd0276a9e56d4899e3a05555
James Dobbe <dobbe2@gmail.com>
2021-08-31 09:05:26 +0000

    tidy up code

--- scenes/battle.js
+++ scenes/battle.js
@@ -4,120 +4,91 @@ let CURRENT_SPEED = INVADER_SPEED;
 const LEVEL_DOWN = 100;
 const STAR_SPEED = 120;
 
-// add([
-// 		text("KILL ALL ALIENS", 18),
-// 		pos(width() / 2, height() / 5 ),
-// 		origin("center"),
-// 		layer("obj"),
-//     'message',
-// 	]);
-
-// action("message", (m) => {
-// 		m.move(0, STAR_SPEED);
-// 		if (m.pos.y >= height()) {
-// 			destroy(m);
-// 		}
-// 	});
-
-
-
 //sound
 const music = play('invaders', { loop: true, });
 volume(0.5)
-// const music2 = play('win');
-// const music3 = play('gameOver');
-
 
 //layers of the game board
 layer(['obj', 'ui'], 'obj')
 
 //sky
-
 const sky = add([
   rect(width(), height()),
-  color(0,0,0,0),
+  color(0, 0, 0, 0),
 ]);
-//stars
+
 //add stars
-  add([
-		sprite("stars"),
-		scale(width() / 880, height() / 880),
-		pos(50, 0),
-		"stars",
-	]);
-
-	add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(100, 80),
-		"stars",
-	]);
-
-  add([
-		sprite("stars"),
-		scale(width() / 880, height() / 880),
-		pos(150, 120),
-		"stars",
-	]);
-
-	add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(200, 260),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 880, height() / 880),
+  pos(50, 0),
+  "stars",
+]);
 
 add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(240, 20),
-		"stars",
-	]);
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(100, 80),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(290, 160),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 880, height() / 880),
+  pos(150, 120),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(360, 360),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(200, 260),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(400, 55),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(240, 20),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 980, height() / 980),
-		pos(420, 400),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(290, 160),
+  "stars",
+]);
 
-  //move stars down the screen
+add([
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(360, 360),
+  "stars",
+]);
+
+add([
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(400, 55),
+  "stars",
+]);
+
+add([
+  sprite("stars"),
+  scale(width() / 980, height() / 980),
+  pos(420, 400),
+  "stars",
+]);
+
+//move stars down the screen
 action("stars", (r) => {
-		r.move(0, STAR_SPEED);
-		if (r.pos.y >= height()) {
-			r.pos.y -= height() * 2;
-		}
-	});
-  
-//message
-// add([
-// 		text("KILL ALL ALIENS",24),
-// 		pos(width() / 2, height() / 2),
-// 		origin("center"),
-// 		layer("ui"),
-// 	]);
-  
+  r.move(0, STAR_SPEED);
+  if (r.pos.y >= height()) {
+    r.pos.y -= height() * 2;
+  }
+});
+
 //create the game board
 addLevel([
   '%                %',
@@ -143,15 +114,14 @@ addLevel([
   '!                &',
   '!                &',
 ], {
-  width: 30,
-  height: 22,
-  '^' : [ sprite('space-invader'), scale(0.5), 'space-invader'],
-  '@' : [ sprite('space-invader2'), scale(0.5), 'space-invader'],
-  '!' : [ sprite('wall'), 'left-wall'],
-  '&' : [ sprite('wall'), 'right-wall'],
-  '%' : [ sprite('leader'), 'left-wall', 'right-wall']
-  
-})
+    width: 30,
+    height: 22,
+    '^': [sprite('space-invader'), scale(0.5), 'space-invader'],
+    '@': [sprite('space-invader2'), scale(0.5), 'space-invader'],
+    '!': [sprite('wall'), 'left-wall'],
+    '&': [sprite('wall'), 'right-wall'],
+    '%': [sprite('leader'), 'left-wall', 'right-wall']
+  })
 
 //add spaceship
 const player = add([
@@ -163,7 +133,7 @@ const player = add([
 //move player left
 keyDown('left', () => {
   player.move(-MOVE_SPEED, 0);
-  if(player.pos.x < 20) {
+  if (player.pos.x < 20) {
     player.pos.x = 20;
   }
 })
@@ -171,21 +141,22 @@ keyDown('left', () => {
 //move player right
 keyDown('right', () => {
   player.move(MOVE_SPEED, 0);
-  if(player.pos.x > width()){
+  if (player.pos.x > width()) {
     player.pos.x = width();
   }
 })
 
 //spawn bullet
-function spawnBullet(p){
+function spawnBullet(p) {
   add([
-    rect(4,12), 
-    pos(p), 
-    origin('center'), 
-    color(rand(rgb(0, 0, 0), rgb(1, 1, 1))), 
+    rect(4, 12),
+    pos(p),
+    origin('center'),
+    color(rand(rgb(0, 0, 0), rgb(1, 1, 1))),
     'bullet'
-    ])
+  ])
 }
+
 //fire bullets
 keyPress('space', () => {
   play('pew');
@@ -196,23 +167,23 @@ const BULLET_SPEED = 400
 //move bullet
 action('bullet', (b) => {
   b.move(0, -BULLET_SPEED)
-  if(b.pos.y < 0) {
+  if (b.pos.y < 0) {
     destroy(b);
   }
 })
 
 //bullet hits invader
-collides('bullet', 'space-invader', (b,s) =>  {
+collides('bullet', 'space-invader', (b, s) => {
   play('splode');
   camShake(4);
   destroy(b);
   destroy(s);
-  score.value ++;
+  score.value++;
   score.text = score.value;
   //clear all aliens, win!
-  if(score.value === 30){
+  if (score.value === 30) {
     music.pause();
-    go('win', { score : score.value })
+    go('win', { score: score.value })
   }
 })
 
@@ -223,15 +194,14 @@ const scoreboard = add([
 ])
 
 const score = add([
-  
   text('0'),
   // origin("top"),   
   pos(150, height() / 99),
   layer('ui'),
   scale(3),
-   {
-     value: 0,
-   }
+  {
+    value: 0,
+  }
 ])
 
 action('space-invader', (s) => {
@@ -242,38 +212,36 @@ action('space-invader', (s) => {
 collides('space-invader', 'right-wall', () => {
   CURRENT_SPEED = -INVADER_SPEED
   every('space-invader', (s) => {
-     s.move(0, LEVEL_DOWN);
+    s.move(0, LEVEL_DOWN);
     music.detune(music.detune() + 1);
   })
 })
 
 //left wall collision and reverse
 collides('space-invader', 'left-wall', () => {
-
   CURRENT_SPEED = INVADER_SPEED
   every('space-invader', (s) => {
-     s.move(0, LEVEL_DOWN)
+    s.move(0, LEVEL_DOWN)
   })
 })
 
 keyPress("m", () => {
-
-	// pause / play music
-	if (music.paused()) {
-		music.play();
-	} else {
-		music.pause();
-	}
+  // pause / play music
+  if (music.paused()) {
+    music.play();
+  } else {
+    music.pause();
+  }
 });
 
 player.overlaps('space-invader', () => {
   music.pause();
-  go('lose', { score : score.value })
+  go('lose', { score: score.value })
 })
 
 action('space-invader', (s) => {
-  if (s.pos.y >= height() ){
+  if (s.pos.y >= height()) {
     music.pause();
-      go('lose', { score : score.value })
+    go('lose', { score: score.value })
   }
 })
--- scenes/insaneBattle.js
+++ scenes/insaneBattle.js
@@ -14,69 +14,67 @@ layer(['obj', 'ui'], 'obj')
 //sky
 const sky = add([
   rect(width(), height()),
-  color(0,0,0,0),
+  color(0, 0, 0, 0),
 ]);
 
-//stars
 //add stars
-  add([
-		sprite("stars"),
-		scale(width() / 580, height() / 580),
-		pos(50, 0),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 580, height() / 580),
+  pos(50, 0),
+  "stars",
+]);
 
-	add([
-		sprite("stars"),
-		scale(width() / 680, height() / 680),
-		pos(100, 80),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 680, height() / 680),
+  pos(100, 80),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 580, height() / 580),
-		pos(150, 120),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 580, height() / 580),
+  pos(150, 120),
+  "stars",
+]);
 
-	add([
-		sprite("stars"),
-		scale(width() / 680, height() / 680),
-		pos(200, 260),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 680, height() / 680),
+  pos(200, 260),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 680, height() / 680),
-		pos(240, 20),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 680, height() / 680),
+  pos(240, 20),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 680, height() / 680),
-		pos(290, 160),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 680, height() / 680),
+  pos(290, 160),
+  "stars",
+]);
 
-  add([
-		sprite("stars"),
-		scale(width() / 680, height() / 680),
-		pos(360, 360),
-		"stars",
-	]);
+add([
+  sprite("stars"),
+  scale(width() / 680, height() / 680),
+  pos(360, 360),
+  "stars",
+]);
 
-  //move stars down the screen
-  action("stars", (r) => {
-      r.move(0, STAR_SPEED);
-      if (r.pos.y >= height()) {
-        r.pos.y -= height() * 2;
-      }
-    });
+//move stars down the screen
+action("stars", (r) => {
+  r.move(0, STAR_SPEED);
+  if (r.pos.y >= height()) {
+    r.pos.y -= height() * 2;
+  }
+});
 
-  
 //create the game board
 addLevel([
   '%                %',
@@ -102,13 +100,13 @@ addLevel([
   '!                &',
   '!                &',
 ], {
-  width: 30,
-  height: 22,
-  '^' : [ sprite('space-invader'), scale(0.5), 'space-invader'],
-  '@' : [ sprite('space-invader2'), scale(0.5), 'space-invader'],
-  '!' : [ sprite('wall'), 'left-wall'],
-  '&' : [ sprite('wall'), 'right-wall'],
-  '%' : [ sprite('leader'), 'left-wall', 'right-wall']
+    width: 30,
+    height: 22,
+    '^': [sprite('space-invader'), scale(0.5), 'space-invader'],
+    '@': [sprite('space-invader2'), scale(0.5), 'space-invader'],
+    '!': [sprite('wall'), 'left-wall'],
+    '&': [sprite('wall'), 'right-wall'],
+    '%': [sprite('leader'), 'left-wall', 'right-wall']
 })
 
 //add spaceship
@@ -121,7 +119,7 @@ const player = add([
 //move player left
 keyDown('left', () => {
   player.move(-MOVE_SPEED, 0);
-  if(player.pos.x < 20) {
+  if (player.pos.x < 20) {
     player.pos.x = 20;
   }
 })
@@ -129,21 +127,22 @@ keyDown('left', () => {
 //move player right
 keyDown('right', () => {
   player.move(MOVE_SPEED, 0);
-  if(player.pos.x > width()){
+  if (player.pos.x > width()) {
     player.pos.x = width();
   }
 })
 
 //spawn bullet
-function spawnBullet(p){
+function spawnBullet(p) {
   add([
-    rect(4,12), 
-    pos(p), 
-    origin('center'), 
-    color(rand(rgb(0, 0, 0), rgb(1, 1, 1))), 
+    rect(4, 12),
+    pos(p),
+    origin('center'),
+    color(rand(rgb(0, 0, 0), rgb(1, 1, 1))),
     'bullet'
-    ])
+  ])
 }
+
 //fire bullets
 keyPress('space', () => {
   play('pew');
@@ -154,23 +153,23 @@ const BULLET_SPEED = 400
 //move bullet
 action('bullet', (b) => {
   b.move(0, -BULLET_SPEED)
-  if(b.pos.y < 0) {
+  if (b.pos.y < 0) {
     destroy(b);
   }
 })
 
 //bullet hits invader
-collides('bullet', 'space-invader', (b,s) =>  {
+collides('bullet', 'space-invader', (b, s) => {
   play('splode');
   camShake(4);
   destroy(b);
   destroy(s);
-  score.value ++;
+  score.value++;
   score.text = score.value;
   //clear all aliens, win!
-  if(score.value === 30){
+  if (score.value === 30) {
     music.pause();
-    go('win', { score : score.value })
+    go('win', { score: score.value })
   }
 })
 
@@ -180,9 +179,9 @@ const score = add([
   pos(4, height() - 2),
   layer('ui'),
   scale(3),
-   {
-     value: 0,
-   }
+  {
+    value: 0,
+  }
 ])
 
 action('space-invader', (s) => {
@@ -193,7 +192,7 @@ action('space-invader', (s) => {
 collides('space-invader', 'right-wall', () => {
   CURRENT_SPEED = -INVADER_SPEED
   every('space-invader', (s) => {
-     s.move(0, LEVEL_DOWN);
+    s.move(0, LEVEL_DOWN);
     music.detune(music.detune() + 1);
   })
 })
@@ -202,27 +201,27 @@ collides('space-invader', 'right-wall', () => {
 collides('space-invader', 'left-wall', () => {
   CURRENT_SPEED = INVADER_SPEED
   every('space-invader', (s) => {
-     s.move(0, LEVEL_DOWN)
+    s.move(0, LEVEL_DOWN)
   })
 })
 
 keyPress("m", () => {
-	// pause / play music
-	if (music.paused()) {
-		music.play();
-	} else {
-		music.pause();
-	}
+  // pause / play music
+  if (music.paused()) {
+    music.play();
+  } else {
+    music.pause();
+  }
 });
 
 player.overlaps('space-invader', () => {
   music.pause();
-  go('lose', { score : score.value })
+  go('lose', { score: score.value })
 })
 
 action('space-invader', (s) => {
-  if (s.pos.y >= height() ){
+  if (s.pos.y >= height()) {
     music.pause();
-      go('lose', { score : score.value })
+    go('lose', { score: score.value })
   }
 })
